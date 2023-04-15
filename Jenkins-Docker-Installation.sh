3d277067cd75a38dd6825b713f271c3cd7952f7b
Santosh Pusparaj <89689096+SantoshPusparaj2109@users.noreply.github.com>
2023-04-15 14:19:21 +0530

    Update Jenkins-Docker-Installation.sh

--- Jenkins-Docker-Installation.sh
+++ Jenkins-Docker-Installation.sh
@@ -5,7 +5,6 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo amazon-linux-extras install java-openjdk11 -y
-sudo dnf install java-11-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
@@ -14,8 +13,12 @@ sudo yum install docker -y
 sudo usermod -a -G docker ec2-user
 id ec2-user
 newgrp docker
+#Installing Docker Compose
 sudo yum install python3-pip
 sudo pip3 install docker-compose
 pip3 install --user docker-compose
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+#Installing Git and Maven
+sudo yum install git -y
+sudo yum install maven -y
